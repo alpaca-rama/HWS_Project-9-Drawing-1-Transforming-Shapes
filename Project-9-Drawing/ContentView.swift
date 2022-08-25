c9e23bc510327ee897190df1d75a0c7da89cbd0c
Luca <luca@weve-got-it.com>
2022-08-25 20:33:02 +0200

    Initial Commit

---
+++ Project-9-Drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-9-Drawing
+//
+//  Created by Luca Capriati on 2022/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
